4a58cd6dd6e06e33ab8e2ac0e8c7a9d233e5fa36
qinshuo <654989417@qq.com>
2018-08-02 12:07:54 +0800

    first commit

---
+++ app/src/main/java/com/tinker/tinkerjdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.tinker.tinkerjdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.tinkerpatch.sdk.TinkerPatch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ((TextView) findViewById(R.id.textview)).setText("have patched..............");
+        TinkerPatch.with().fetchPatchUpdate(true);
+        ((Button) findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TinkerPatch.with().fetchPatchUpdate(true);
+            }
+        });
+
+        ((Button) findViewById(R.id.button)).setText("成功率也太低了");
+    }
+}
